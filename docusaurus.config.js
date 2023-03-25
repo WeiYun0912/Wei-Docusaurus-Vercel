9c7ed65a2144bbb66d4b20d94eabdd9f411fca65
ChunWeiYeah <yher25@gmail.com>
2023-03-25 13:13:19 +0800

    update theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
       ],
       colorMode: {
         defaultMode: "dark",
-        disableSwitch: false,
+        disableSwitch: true,
         respectPrefersColorScheme: false,
       },
       announcementBar: {
