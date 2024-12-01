9e073c77467f9f075dc642be07feb94378cb0522
WeiWei <yher25@gmail.com>
2024-12-01 13:15:59 +0800

    fix react not found error

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,7 +1,4 @@
-import { useIsHomePage } from "@site/src/hooks/useIsHomePage";
-
+import React from "react";
 export default function Layout(props) {
-    const isHomePage = useIsHomePage();
-
-    return <div data-page-type={isHomePage ? "home" : "other"}>{/* ... 其他代碼 */}</div>;
+    return <div>{props.children}</div>;
 }
